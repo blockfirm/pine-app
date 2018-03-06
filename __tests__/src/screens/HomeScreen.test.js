4dd722b8967046ac9da2c750de011c230090b778
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-03-07 00:17:10 +0100

    Test src/screens/HomeScreen.js

---
+++ __tests__/src/screens/HomeScreen.test.js
@@ -0,0 +1,14 @@
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import HomeScreen from '../../../src/screens/HomeScreen';
+
+describe('HomeScreen', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(
+      <HomeScreen />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
