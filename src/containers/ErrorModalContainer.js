50263f358d5b91acf3e51829804063ae5269838e
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-03-15 00:30:45 -0400

    Create an ErrorModalContainer that shows errors from the state

---
+++ src/containers/ErrorModalContainer.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { dismissError } from '../actions';
+import ErrorModal from '../components/ErrorModal';
+
+const mapStateToProps = (state) => {
+  return {
+    error: state.error
+  };
+};
+
+class ErrorModalContainer extends Component {
+  static propTypes = {
+    dispatch: PropTypes.func
+  };
+
+  _onDismiss() {
+    const dispatch = this.props.dispatch;
+    dispatch(dismissError());
+  }
+
+  render() {
+    return (
+      <ErrorModal
+        {...this.props}
+        onDismiss={this._onDismiss.bind(this)}
+      />
+    );
+  }
+}
+
+const ErrorModalConnector = connect(
+  mapStateToProps
+)(ErrorModalContainer);
+
+export default ErrorModalConnector;
