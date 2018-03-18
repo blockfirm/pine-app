9ca9e0b3f330597210a3adad95efe875ab40b3af
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-03-17 23:29:17 -0400

    Add HeaderContainer component
    This container component will show the back button in the header
    if there is any previous history to navigate to.

---
+++ src/containers/HeaderContainer.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { NavigationActions } from 'react-navigation';
+import Header from '../components/Header';
+
+const mapStateToProps = (state) => {
+  return {
+    nav: state.nav
+  };
+};
+
+class HeaderContainer extends Component {
+  static propTypes = {
+    nav: PropTypes.object,
+    dispatch: PropTypes.func,
+    backButtonIconStyle: PropTypes.any
+  };
+
+  _onBackPress() {
+    const dispatch = this.props.dispatch;
+    dispatch(NavigationActions.back());
+  }
+
+  _shouldShowBackButton() {
+    // Show back button if there is navigation history.
+    const nav = this.props.nav.routes[0];
+    const showBackButton = nav.index > 0;
+
+    return showBackButton;
+  }
+
+  render() {
+    const showBackButton = this._shouldShowBackButton();
+
+    return (
+      <Header
+        showBackButton={showBackButton}
+        onBackPress={this._onBackPress.bind(this)}
+        backButtonIconStyle={this.props.backButtonIconStyle}
+      />
+    );
+  }
+}
+
+const HeaderConnector = connect(
+  mapStateToProps
+)(HeaderContainer);
+
+export default HeaderConnector;
