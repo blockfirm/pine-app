0f079aac95744f3125c9ecc926c85094f3d136b7
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-03-20 22:30:42 -0400

    Add Link component

---
+++ __tests__/src/components/Link.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text } from 'react-native';
+import renderer from 'react-test-renderer';
+import Link from '../../../src/components/Link';
+
+describe('Link', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(
+      <Link>
+        <Text>e80d5b55-cad9-4df9-9272-c0398592411b</Text>
+      </Link>
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
---
+++ src/components/Link.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { StyleSheet, TouchableOpacity, Text } from 'react-native';
+import PropTypes from 'prop-types';
+import StyledText from './StyledText';
+
+const styles = StyleSheet.create({
+  link: {
+    fontSize: 12,
+    fontWeight: '600',
+    color: '#007AFF'
+  }
+});
+
+export default class Link extends Component {
+  render() {
+    return (
+      <TouchableOpacity onPress={this.props.onPress}>
+        <Text>
+          <StyledText style={[styles.link, this.props.style]}>
+            {this.props.children}
+          </StyledText>
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+Link.propTypes = {
+  onPress: PropTypes.func,
+  style: PropTypes.any,
+  children: PropTypes.node
+};
