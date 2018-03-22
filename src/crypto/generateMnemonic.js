d4ae3114d3bda1c4ebf4e12c18d065956704e226
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-03-22 01:17:43 -0400

    Add comment to generateMnemonic about randomBytes

--- src/crypto/generateMnemonic.js
+++ src/crypto/generateMnemonic.js
@@ -4,6 +4,12 @@ import randomBytes from './randomBytes';
 const ENTROPY = 128; // 128-bit entropy equals 12 mnemonic words.
 
 export default function generateMnemonic() {
+  /*
+   * This function first generates the random bytes and then passes
+   * them to the bip39.generateMnemonic method. The reason for this
+   * is that bip39.generateMnemonic otherwise calls randomBytes
+   * synchronously which then won't use iOS's SecRandomCopyBytes.
+   */
   return randomBytes(ENTROPY / 8).then((bytes) => {
     const getRandomBytes = () => bytes;
     const wordlist = bip39.english;
