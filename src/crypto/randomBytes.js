bc735d919e59ac7338488f2e044d78797784f0e9
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-03-22 01:01:08 -0400

    Create a promise wrapper for randomBytes
    It's very important to keep the call to randomBytes asynchronous.
    The synchronous version of randomBytes will not use iOS's
    SecRandomCopyBytes. Instead it will use the SJCL library which is
    not secure enough for private key generation.
    
    See more here:
    https://github.com/mvayngrib/react-native-randombytes

---
+++ src/crypto/randomBytes.js
@@ -0,0 +1,19 @@
+import { randomBytes } from 'react-native-randombytes';
+
+export default function (size) {
+  return new Promise((resolve, reject) => {
+    /*
+     * It's very important to keep this call asynchronous. The synchronous version
+     * of randomBytes will not use iOS's SecRandomCopyBytes. Instead it will use
+     * the SJCL library which is not secure enough for private key generation.
+     * <https://github.com/mvayngrib/react-native-randombytes>
+     */
+    randomBytes(size, (error, bytes) => {
+      if (error) {
+        return reject(error);
+      }
+
+      resolve(bytes);
+    });
+  });
+}
