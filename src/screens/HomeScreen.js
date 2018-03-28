9be4d5b02a574b51034316985bfad72f4625fe04
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-03-29 01:44:23 +0200

    Remove boilerplate code from HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,53 +1,15 @@
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet
-} from 'react-native';
-
-import StyledText from '../components/StyledText';
-import Paragraph from '../components/Paragraph';
+import Title from '../components/Title';
 import BaseScreen from './BaseScreen';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class HomeScreen extends Component<Props> {
+export default class HomeScreen extends Component {
   render() {
     return (
-      <BaseScreen style={styles.container}>
-        <StyledText style={styles.welcome}>
-          Welcome to React Native!
-        </StyledText>
-        <Paragraph style={styles.instructions}>
-          To get started, edit App.js
-        </Paragraph>
-        <Paragraph style={styles.instructions}>
-          {instructions}
-        </Paragraph>
+      <BaseScreen>
+        <Title>
+          Welcome to Wallet ID!
+        </Title>
       </BaseScreen>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    marginBottom: 5
-  }
-});
