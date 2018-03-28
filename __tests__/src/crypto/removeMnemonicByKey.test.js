ab1b10c0adb27808c96a6502b438639cdb442d4a
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-03-29 00:34:39 +0200

    Test removeMnemonicByKey.js

---
+++ __tests__/src/crypto/removeMnemonicByKey.test.js
@@ -0,0 +1,32 @@
+import * as Keychain from 'react-native-keychain';
+import removeMnemonicByKey from '../../../src/crypto/removeMnemonicByKey';
+
+describe('removeMnemonicByKey', () => {
+  beforeEach(() => {
+    Keychain.resetGenericPassword.mockClear();
+  });
+
+  it('is a function', () => {
+    expect(typeof removeMnemonicByKey).toBe('function');
+  });
+
+  it('accepts one argument', () => {
+    expect(removeMnemonicByKey.length).toBe(1);
+  });
+
+  it('calls Keychain.resetGenericPassword with service set to the key id', () => {
+    const fakeKeyId = 'c9103642-6a6e-46ca-ad74-30acb61c238a';
+    removeMnemonicByKey(fakeKeyId);
+    expect(Keychain.resetGenericPassword).toBeCalledWith({ service: fakeKeyId });
+  });
+
+  it('returns the result from Keychain.resetGenericPassword', () => {
+    Keychain.resetGenericPassword.mockImplementationOnce(() => {
+      return 'd8de749d-0773-4f36-955c-25cdb9c2712b';
+    });
+
+    const returnValue = removeMnemonicByKey();
+
+    expect(returnValue).toBe('d8de749d-0773-4f36-955c-25cdb9c2712b');
+  });
+});
