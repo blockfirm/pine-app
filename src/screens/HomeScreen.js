b5d712680b976de2a4af11bbb1ba94ca5c706f28
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-03-29 02:15:18 +0200

    Add Settings button to Home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 import Title from '../components/Title';
 import BaseScreen from './BaseScreen';
 
+const styles = StyleSheet.create({
+  navigationIcon: {
+    fontSize: 28,
+    color: '#017AFF',
+    padding: 10
+  }
+});
+
 export default class HomeScreen extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    headerLeft: (
+      <TouchableOpacity onPress={() => { navigation.navigate('Settings'); }}>
+        <Icon name='ios-settings-outline' style={styles.navigationIcon} />
+      </TouchableOpacity>
+    )
+  });
+
   render() {
     return (
       <BaseScreen>
