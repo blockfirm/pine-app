8a1e898a26472820de8b74cea0fb9046cdb1d833
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-07-30 18:31:10 +0200

    Upgrade bitcoinjs-lib to v4 from v3

--- src/crypto/getPublicKeyFromMnemonic.js
+++ src/crypto/getPublicKeyFromMnemonic.js
@@ -1,9 +1,9 @@
-import bitcoin from 'bitcoinjs-lib';
+import bip32 from 'bip32';
 import bip39 from 'bip39';
 
 const getPublicKeyFromMnemonic = (mnemonic) => {
   const seed = bip39.mnemonicToSeed(mnemonic);
-  const masterNode = bitcoin.HDNode.fromSeedBuffer(seed);
+  const masterNode = bip32.fromSeed(seed);
   const xpub = masterNode.neutered().toBase58();
 
   return xpub;
