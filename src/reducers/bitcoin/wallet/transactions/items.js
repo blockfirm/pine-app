c6f78e78a9db2add792003fbc279aa7a6d5fd00a
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-08-06 23:47:20 +0200

    Sort all transactions everytime new transactions are added

--- src/reducers/bitcoin/wallet/transactions/items.js
+++ src/reducers/bitcoin/wallet/transactions/items.js
@@ -1,15 +1,22 @@
 import * as transactionActions from '../../../../actions/bitcoin/wallet/transactions';
 
 const itemsReducer = (state = [], action) => {
+  let newState;
+
   switch (action.type) {
     case transactionActions.BITCOIN_WALLET_TRANSACTIONS_LOAD_SUCCESS:
       return action.transactions;
 
     case transactionActions.BITCOIN_WALLET_TRANSACTIONS_ADD_SUCCESS:
-      return [
+      newState = [
         ...state,
         ...action.transactions
       ];
+
+      // Sort ascending on time.
+      newState.sort((a, b) => a.time - b.time);
+
+      return newState;
 
     case transactionActions.BITCOIN_WALLET_TRANSACTIONS_REMOVE_ALL_SUCCESS:
       return [];
