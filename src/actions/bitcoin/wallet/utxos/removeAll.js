6c13c187e053883282c907ca7c1f08b731cf42c7
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-09-16 23:41:35 +0200

    Create action to remove all utxos

---
+++ src/actions/bitcoin/wallet/utxos/removeAll.js
@@ -0,0 +1,24 @@
+import { save } from './save';
+
+export const BITCOIN_WALLET_UTXOS_REMOVE_ALL_SUCCESS = 'BITCOIN_WALLET_UTXOS_REMOVE_ALL_SUCCESS';
+
+const removeAllSuccess = () => {
+  return {
+    type: BITCOIN_WALLET_UTXOS_REMOVE_ALL_SUCCESS
+  };
+};
+
+/**
+ * Action to remove all utxos. Useful when resetting the app/wallet.
+ */
+export const removeAll = () => {
+  return (dispatch) => {
+    /**
+     * The utxos are removed to the state by the reducer,
+     * so this action must be dispatched before saving the state.
+     */
+    dispatch(removeAllSuccess());
+
+    return dispatch(save());
+  };
+};
