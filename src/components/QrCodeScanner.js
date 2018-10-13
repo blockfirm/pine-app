96261420f83937e7d267c4f230e208bbe02f29c7
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-10-13 23:32:08 +0200

    Handle camera when not authorized

--- src/components/QrCodeScanner.js
+++ src/components/QrCodeScanner.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Image, Dimensions } from 'react-native';
+import { StyleSheet, View, Image, Dimensions, Linking } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import PropTypes from 'prop-types';
 import { RNCamera } from 'react-native-camera';
+
+import Link from '../components/Link';
 import getStatusBarHeight from '../utils/getStatusBarHeight';
 
 const windowHeight = Dimensions.get('window').height;
@@ -12,15 +14,16 @@ const styles = StyleSheet.create({
   view: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#000000'
+    alignItems: 'center'
   },
   camera: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     height: windowHeight,
-    width: windowWidth
+    width: windowWidth,
+    position: 'absolute',
+    backgroundColor: '#000000'
   },
   viewport: {
     width: 212,
@@ -37,7 +40,15 @@ const styles = StyleSheet.create({
 });
 
 export default class QrCodeScanner extends Component {
+  state = {
+    cameraReady: false
+  }
+
   shouldComponentUpdate(nextProps) {
+    if (!this.state.cameraReady) {
+      return true;
+    }
+
     if (nextProps.showPreview === this.props.showPreview) {
       return true;
     }
@@ -51,13 +62,47 @@ export default class QrCodeScanner extends Component {
     return false;
   }
 
+  _goToAppSettings() {
+    Linking.openURL('app-settings:');
+  }
+
+  _onCameraReady() {
+    this.setState({
+      cameraReady: true
+    });
+  }
+
+  _renderNotAuthorizedView() {
+    return (
+      <Link onPress={this._goToAppSettings.bind(this)}>Enable Camera Access</Link>
+    );
+  }
+
+  _renderCameraContent() {
+    if (!this.state.cameraReady) {
+      return;
+    }
+
+    return (
+      <View style={styles.view}>
+        <LinearGradient colors={['rgba(0, 0, 0, 0.3)', 'rgba(0, 0, 0, 0.0)']} style={styles.topGradient} />
+        <Image source={require('../images/QRViewport.png')} style={styles.viewport} />
+      </View>
+    );
+  }
+
   render() {
     return (
       <View style={styles.view}>
-        <RNCamera ref={(ref) => { this._camera = ref; }} style={styles.camera} onBarCodeRead={this.props.onScan}>
-          <LinearGradient colors={['rgba(0, 0, 0, 0.3)', 'rgba(0, 0, 0, 0.0)']} style={styles.topGradient} />
-          <Image source={require('../images/QRViewport.png')} style={styles.viewport} />
-        </RNCamera>
+        <RNCamera
+          ref={(ref) => {
+            this._camera = ref;
+          }}
+          style={styles.camera}
+          onCameraReady={this._onCameraReady.bind(this)}
+          notAuthorizedView={this._renderNotAuthorizedView()}
+        />
+        { this._renderCameraContent() }
       </View>
     );
   }
