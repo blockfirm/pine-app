d6375b2630492e98cfb8687cc30f3d85cfe9f62e
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-11-08 22:37:45 +0100

    Use FlatList instead of react-native-swiper
    react-native-swiper is no longer maintained and contains a lot of bugs.
    FlatList is a standard React Native component that seems to be working
    exactly the same for this use-case but without the extra dependency.
    
    At the same time, this commit also makes sure to pause the camera
    preview when it's not active or being swiped to.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { StatusBar, NetInfo } from 'react-native';
+import { StatusBar, NetInfo, View, StyleSheet, FlatList, Dimensions } from 'react-native';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Swiper from 'react-native-swiper';
 
 import { sync as syncWallet } from '../actions/bitcoin/wallet';
 import TransactionsScreen from './TransactionsScreen';
@@ -10,6 +9,21 @@ import CameraScreen from './CameraScreen';
 import ReceiveScreen from './ReceiveScreen';
 
 const SYNC_WALLET_INTERVAL = 60 * 1000 * 1; // 1 minute.
+const DEFAULT_SCREEN_INDEX = 1;
+
+const WINDOW_HEIGHT = Dimensions.get('window').height;
+const WINDOW_WIDTH = Dimensions.get('window').width;
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    alignSelf: 'stretch'
+  },
+  screen: {
+    width: WINDOW_WIDTH,
+    height: WINDOW_HEIGHT
+  }
+});
 
 @connect()
 export default class HomeScreen extends Component {
@@ -17,7 +31,10 @@ export default class HomeScreen extends Component {
     header: null
   }
 
-  state = {}
+  state = {
+    activeIndex: DEFAULT_SCREEN_INDEX,
+    scrollingToIndex: DEFAULT_SCREEN_INDEX
+  }
 
   componentDidMount() {
     const dispatch = this.props.dispatch;
@@ -55,26 +72,78 @@ export default class HomeScreen extends Component {
     const barStyle = index === 0 ? 'light-content' : 'dark-content';
 
     StatusBar.setBarStyle(barStyle);
+    this.setState({ activeIndex: index });
+  }
 
-    this.setState({
-      activeIndex: index
-    });
+  _onMomentumScrollEnd(event) {
+    const { contentOffset, layoutMeasurement } = event.nativeEvent;
+    const activeIndex = Math.floor(contentOffset.x / layoutMeasurement.width);
+
+    if (activeIndex !== this.state.activeIndex) {
+      this._onIndexChanged(activeIndex);
+    }
+  }
+
+  _onScroll(event) {
+    const activeIndex = this.state.activeIndex;
+    const { contentOffset, layoutMeasurement } = event.nativeEvent;
+    const previousOffsetX = layoutMeasurement.width * this.state.activeIndex;
+    let scrollingToIndex;
+
+    if (previousOffsetX > contentOffset.x) {
+      scrollingToIndex = activeIndex - 1;
+    } else if (previousOffsetX < contentOffset.x) {
+      scrollingToIndex = activeIndex + 1;
+    } else {
+      scrollingToIndex = activeIndex;
+    }
+
+    this.setState({ scrollingToIndex });
+  }
+
+  _renderScreen({ item }) {
+    return (
+      <View style={styles.screen}>
+        {item.screen}
+      </View>
+    );
+  }
+
+  _shouldShowCameraPreview() {
+    const { activeIndex, scrollingToIndex } = this.state;
+    const showCameraPreview = activeIndex === 0 || scrollingToIndex === 0;
+
+    return showCameraPreview;
   }
 
   render() {
-    const showCameraPreview = this.state.activeIndex === 0;
+    const showCameraPreview = this._shouldShowCameraPreview();
+
+    const screens = [
+      { key: 'camera', screen: <CameraScreen showPreview={showCameraPreview} /> },
+      { key: 'transactions', screen: <TransactionsScreen /> },
+      { key: 'receive', screen: <ReceiveScreen /> }
+    ];
 
     return (
-      <Swiper
-        index={1}
-        loop={false}
-        showsPagination={false}
-        onIndexChanged={this._onIndexChanged.bind(this)}
-      >
-        <CameraScreen showPreview={showCameraPreview} />
-        <TransactionsScreen />
-        <ReceiveScreen />
-      </Swiper>
+      <FlatList
+        bounces={false}
+        getItemLayout={(data, index) => ({
+          length: WINDOW_WIDTH,
+          offset: WINDOW_WIDTH * index,
+          index
+        })}
+        showsHorizontalScrollIndicator={false}
+        pagingEnabled={true}
+        horizontal={true}
+        style={styles.view}
+        data={screens}
+        renderItem={this._renderScreen}
+        initialScrollIndex={DEFAULT_SCREEN_INDEX}
+        onMomentumScrollEnd={this._onMomentumScrollEnd.bind(this)}
+        onScroll={this._onScroll.bind(this)}
+        scrollEventThrottle={16}
+      />
     );
   }
 }
