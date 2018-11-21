60fb5c40df889652692fe7c272b6486a6d38600e
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-11-21 23:28:40 +0100

    Create function for getting payment info from a URI or address

---
+++ __tests__/src/crypto/bitcoin/getPaymentInfoFromString.test.js
@@ -0,0 +1,86 @@
+import getPaymentInfoFromString from '../../../../src/crypto/bitcoin/getPaymentInfoFromString';
+
+describe('getPaymentInfoFromString', () => {
+  it('is a function', () => {
+    expect(typeof getPaymentInfoFromString).toBe('function');
+  });
+
+  it('accepts two arguments', () => {
+    expect(getPaymentInfoFromString.length).toBe(2);
+  });
+
+  it('returns undefined if the passed string is empty', () => {
+    const paymentInfo = getPaymentInfoFromString('', 'testnet');
+    expect(paymentInfo).toBe(undefined);
+  });
+
+  it('returns undefined if the passed string is undefined', () => {
+    const paymentInfo = getPaymentInfoFromString(undefined, 'testnet');
+    expect(paymentInfo).toBe(undefined);
+  });
+
+  it('returns undefined if the passed string is not a string', () => {
+    const paymentInfo = getPaymentInfoFromString({}, 'testnet');
+    expect(paymentInfo).toBe(undefined);
+  });
+
+  describe('when the string is a bitcoin URI', () => {
+    it('returns an object with address and amount', () => {
+      const uri = 'bitcoin:2MvQNs9R4qwbfiBaEtbCq8EgUfUxhccHyqm?amount=1.28';
+      const paymentInfo = getPaymentInfoFromString(uri, 'testnet');
+
+      expect(paymentInfo).toMatchObject({
+        address: '2MvQNs9R4qwbfiBaEtbCq8EgUfUxhccHyqm',
+        amount: 1.28
+      });
+    });
+
+    it('returns undefined when address is invalid', () => {
+      const uri = 'bitcoin:..,.,.?amount=1.28';
+      const paymentInfo = getPaymentInfoFromString(uri, 'testnet');
+
+      expect(paymentInfo).toBeUndefined();
+    });
+
+    it('returns undefined when amount is invalid', () => {
+      const uri = 'bitcoin:2MvQNs9R4qwbfiBaEtbCq8EgUfUxhccHyqm?amount=-1.28';
+      const paymentInfo = getPaymentInfoFromString(uri, 'testnet');
+
+      expect(paymentInfo).toBeUndefined();
+    });
+
+    it('returns amount as undefined when no amount is specified', () => {
+      const uri = 'bitcoin:2MvQNs9R4qwbfiBaEtbCq8EgUfUxhccHyqm';
+      const paymentInfo = getPaymentInfoFromString(uri, 'testnet');
+
+      expect(paymentInfo.address).toBe('2MvQNs9R4qwbfiBaEtbCq8EgUfUxhccHyqm');
+      expect(paymentInfo.amount).toBeUndefined();
+    });
+  });
+
+  describe('when the string is a bitcoin address', () => {
+    it('returns an object with address and amount as undefined', () => {
+      const address = '2MvQNs9R4qwbfiBaEtbCq8EgUfUxhccHyqm';
+      const paymentInfo = getPaymentInfoFromString(address, 'testnet');
+
+      expect(paymentInfo.address).toBe('2MvQNs9R4qwbfiBaEtbCq8EgUfUxhccHyqm');
+      expect(paymentInfo.amount).toBeUndefined();
+    });
+  });
+
+  describe('when the bitcoin address is for the wrong network', () => {
+    it('returns undefined', () => {
+      const address = '2MvQNs9R4qwbfiBaEtbCq8EgUfUxhccHyqm';
+      const paymentInfo = getPaymentInfoFromString(address, 'mainnet');
+
+      expect(paymentInfo).toBeUndefined();
+    });
+  });
+
+  describe('when the string is not a bitcoin URI or address', () => {
+    it('returns undefined', () => {
+      const paymentInfo = getPaymentInfoFromString('964aba8b-cf23-49bd-99fe-93afcad61654', 'testnet');
+      expect(paymentInfo).toBeUndefined();
+    });
+  });
+});
---
+++ src/crypto/bitcoin/getPaymentInfoFromString.js
@@ -0,0 +1,56 @@
+import bip21 from 'bip21';
+import validateAddress from './validateAddress';
+
+const decodeBitcoinUri = (uri) => {
+  let decoded;
+
+  try {
+    decoded = bip21.decode(uri);
+  } catch {
+    return {
+      address: undefined,
+      amount: undefined
+    };
+  }
+
+  return {
+    address: decoded.address,
+    amount: decoded.options.amount
+  };
+};
+
+/**
+ * Parses a string and extracts a valid bitcoin address and amount, if present.
+ *
+ * @param {string} string - A bitcoin URI (BIP21) or address.
+ * @param {string} network - 'mainnet' or 'testnet'.
+ *
+ * @returns { address, amount } A valid bitcoin address and if available, an amount in BTC.
+ */
+const getPaymentInfoFromString = (string, network) => {
+  let address;
+  let amount;
+
+  if (!string || typeof string !== 'string') {
+    return;
+  }
+
+  if (string.search(/^bitcoin/i) === 0) {
+    // Try to decode the bitcoin URI.
+    const decoded = decodeBitcoinUri(string);
+    address = decoded.address;
+    amount = decoded.amount;
+  } else {
+    // Assume the string is an address.
+    address = string;
+  }
+
+  // Validate the address.
+  if (!validateAddress(address, network)) {
+    return;
+  }
+
+  return { address, amount };
+};
+
+export default getPaymentInfoFromString;
