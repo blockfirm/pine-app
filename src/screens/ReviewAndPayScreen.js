ce4c9b733411f3bcb6f352494b53aa9565174aee
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-11-25 22:09:35 +0100

    Select UTXOs and create a transaction
    Uses bitcoinjs to create the transaction and coinselect to select
    the UTXOs to be used as inputs.
    
    TODO: Derive the amount and fee from the created transaction to make
    sure the UI reflects the built transaction. This is to prevent
    displaying an incorrect value in case there are any bugs in the coin
    selection algorithm or transaction builder.

--- src/screens/ReviewAndPayScreen.js
+++ src/screens/ReviewAndPayScreen.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ActivityIndicator } from 'react-native';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import bitcoin from 'bitcoinjs-lib';
+import coinSelect from 'coinselect';
 
+import {
+  UNIT_BTC,
+  UNIT_MBTC,
+  UNIT_SATOSHIS,
+  convert as convertBitcoin
+} from '../crypto/bitcoin/convert';
+
+import { handle as handleError } from '../actions/error/handle';
+import { getEstimate as getFeeEstimate } from '../actions/bitcoin/fees';
 import headerStyles from '../styles/headerStyles';
 import Button from '../components/Button';
 import BackButton from '../components/BackButton';
@@ -17,11 +28,20 @@ const styles = StyleSheet.create({
     padding: 0
   },
   content: {
-    justifyContent: 'flex-start'
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start'
   }
 });
 
-@connect()
+const getBitcoinNetwork = (network) => {
+  return network === 'testnet' ? bitcoin.networks.testnet : bitcoin.networks.mainnet;
+};
+
+@connect((state) => ({
+  utxos: state.bitcoin.wallet.utxos.items,
+  changeAddress: state.bitcoin.wallet.addresses.internal.unused,
+  network: state.settings.bitcoin.network
+}))
 export default class ReviewAndPayScreen extends Component {
   static navigationOptions = ({ navigation }) => {
     return {
@@ -33,8 +53,84 @@ export default class ReviewAndPayScreen extends Component {
     };
   };
 
+  state = {
+    transaction: null,
+    fee: null
+  }
+
+  componentDidMount() {
+    const dispatch = this.props.dispatch;
+
+    // Get transaction fee estimate.
+    dispatch(getFeeEstimate())
+      .then((satoshisPerByte) => {
+        // Create a transaction.
+        this._createTransaction(satoshisPerByte);
+      })
+      .catch((error) => {
+        dispatch(handleError(error));
+      });
+  }
+
+  _getUtxos() {
+    return this.props.utxos.map((utxo) => {
+      const satoshis = convertBitcoin(utxo.value, UNIT_BTC, UNIT_SATOSHIS);
+
+      return {
+        txId: utxo.txid,
+        vout: utxo.n,
+        value: satoshis
+      };
+    });
+  }
+
+  _selectUtxos(satoshisPerByte) {
+    const feeRate = Math.round(satoshisPerByte) || 1; // coinSelect doesn't support decimal fee rates.
+    const { address, amountBtc } = this.props.navigation.state.params;
+    const satoshis = convertBitcoin(amountBtc, UNIT_BTC, UNIT_SATOSHIS);
+    const utxos = this._getUtxos();
+
+    const targets = [{
+      address,
+      value: satoshis
+    }];
+
+    const { inputs, outputs, fee } = coinSelect(utxos, targets, feeRate);
+
+    // No solution was found.
+    if (!inputs || !outputs) {
+      return;
+    };
+
+    return { inputs, outputs, fee };
+  }
+
+  _createTransaction(satoshisPerByte) {
+    const { changeAddress } = this.props;
+    const { inputs, outputs, fee } = this._selectUtxos(satoshisPerByte);
+    const bitcoinNetwork = getBitcoinNetwork(this.props.network);
+    const transactionBuilder = new bitcoin.TransactionBuilder(bitcoinNetwork);
+
+    inputs.forEach((input) => {
+      transactionBuilder.addInput(input.txId, input.vout);
+    });
+
+    outputs.forEach((output) => {
+      output.address = output.address || changeAddress;
+      transactionBuilder.addOutput(output.address, output.value);
+    });
+
+    this.setState({
+      transaction: transactionBuilder,
+      fee
+    });
+  }
+
   render() {
     const { address, amountBtc, displayUnit } = this.props.navigation.state.params;
+    const { transaction, fee } = this.state;
+    const feeBtc = fee ? convertBitcoin(fee, UNIT_SATOSHIS, UNIT_BTC) : 0;
+    const totalAmount = amountBtc + feeBtc;
 
     return (
       <BaseScreen hideHeader={true} style={styles.view}>
@@ -47,9 +143,20 @@ export default class ReviewAndPayScreen extends Component {
               Amount: <BtcLabel amount={amountBtc} unit={displayUnit} />
             </StyledText>
           </View>
+          <View>
+            <StyledText>Fee: </StyledText>
+            {
+              feeBtc ? <BtcLabel amount={feeBtc} unit={displayUnit} /> : <ActivityIndicator size='small' />
+            }
+          </View>
+          <View>
+            <StyledText>
+              Total: <BtcLabel amount={totalAmount} unit={displayUnit} />
+            </StyledText>
+          </View>
         </ContentView>
         <Footer>
-          <Button label='Pay' onPress={() => {}} />
+          <Button label='Pay' disabled={!Boolean(transaction)} onPress={() => {}} />
         </Footer>
       </BaseScreen>
     );
@@ -58,5 +165,8 @@ export default class ReviewAndPayScreen extends Component {
 
 ReviewAndPayScreen.propTypes = {
   dispatch: PropTypes.func,
-  navigation: PropTypes.object
+  navigation: PropTypes.object,
+  utxos: PropTypes.array,
+  changeAddress: PropTypes.string,
+  network: PropTypes.string
 };
