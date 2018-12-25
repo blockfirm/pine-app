3e09eb1574eb33cf682297ab9692c035d0cccb5d
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2018-12-25 16:38:38 +0100

    Sync wallet when a push notification is received

--- src/services/NotificationService.js
+++ src/services/NotificationService.js
@@ -2,6 +2,7 @@ import { PushNotificationIOS } from 'react-native';
 import { onRegister } from '../actions/notifications/onRegister';
 import { onRegisterError } from '../actions/notifications/onRegisterError';
 import { setPermissions } from '../actions/notifications/setPermissions';
+import { sync as syncWallet } from '../actions/bitcoin/wallet';
 
 export default class NotificationService {
   constructor(store) {
@@ -9,6 +10,7 @@ export default class NotificationService {
 
     this._onRegister = this._onRegister.bind(this);
     this._onRegisterError = this._onRegisterError.bind(this);
+    this._onNotification = this._onNotification.bind(this);
   }
 
   start() {
@@ -16,6 +18,7 @@ export default class NotificationService {
 
     PushNotificationIOS.addEventListener('register', this._onRegister);
     PushNotificationIOS.addEventListener('registrationError', this._onRegisterError);
+    PushNotificationIOS.addEventListener('notification', this._onNotification);
 
     this._register();
   }
@@ -23,6 +26,7 @@ export default class NotificationService {
   stop() {
     PushNotificationIOS.removeEventListener('register', this._onRegister);
     PushNotificationIOS.removeEventListener('registrationError', this._onRegisterError);
+    PushNotificationIOS.removeEventListener('notification', this._onNotification);
   }
 
   _register() {
@@ -41,5 +45,15 @@ export default class NotificationService {
   _onRegisterError(error) {
     const { store } = this;
     store.dispatch(onRegisterError(error));
+  }
+
+  _onNotification() {
+    const { store } = this;
+    const state = store.getState();
+    const { initialized } = state.settings;
+
+    if (initialized) {
+      store.dispatch(syncWallet());
+    }
   }
 }
