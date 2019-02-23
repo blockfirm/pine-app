30e93643779afc8791d5d4779ac4ccd7e7fc4d9f
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2019-02-23 23:27:48 +0100

    Test and document PinePaymentProtocol/user/contactRequests/get.js

---
+++ __tests__/src/PinePaymentProtocol/user/contactRequests/get.test.js
@@ -0,0 +1,108 @@
+import getContactRequests from '../../../../../src/PinePaymentProtocol/user/contactRequests/get';
+
+global.fetch = jest.fn(() => Promise.resolve({
+  ok: true,
+  json: () => Promise.resolve([
+    {
+      id: '8f4890a6-2641-4fd1-a5df-bad5b52245fe'
+    }
+  ])
+}));
+
+describe('get', () => {
+  beforeEach(() => {
+    fetch.mockClear();
+  });
+
+  it('is a function', () => {
+    expect(typeof getContactRequests).toBe('function');
+  });
+
+  it('accepts two arguments', () => {
+    expect(getContactRequests.length).toBe(2);
+  });
+
+  describe('when getting contact requests', () => {
+    let pineAddress;
+    let mnemonic;
+    let resolvedValue;
+
+    beforeEach(() => {
+      pineAddress = 'test@pine.cash';
+      mnemonic = 'test boss fly battle rubber wasp afraid party whale hamster chicken vibrant';
+
+      return getContactRequests(pineAddress, mnemonic).then((value) => {
+        resolvedValue = value;
+      });
+    });
+
+    it('resolves to the response from the API', () => {
+      expect(Array.isArray(resolvedValue)).toBe(true);
+      expect(resolvedValue[0]).toBeTruthy();
+      expect(resolvedValue[0].id).toBe('8f4890a6-2641-4fd1-a5df-bad5b52245fe'); // Mocked at the top.
+    });
+
+    describe('the HTTP request', () => {
+      it('is made to the url https://_pine.pine.cash/v1/users/ACjpRHFv7L8iN4qnVeR4U7pyhzGxSr4Z2/contact-requests', () => {
+        const expectedUrl = 'https://_pine.pine.cash/v1/users/ACjpRHFv7L8iN4qnVeR4U7pyhzGxSr4Z2/contact-requests';
+        expect(fetch).toHaveBeenCalledWith(expectedUrl, expect.anything());
+      });
+
+      describe('the request options', () => {
+        let options;
+
+        beforeEach(() => {
+          options = fetch.mock.calls[0][1];
+        });
+
+        it('is an object', () => {
+          expect(typeof options).toBe('object');
+          expect(options).toBeTruthy();
+        });
+
+        it('has "method" set to "GET"', () => {
+          expect(options.method).toBe('GET');
+        });
+
+        it('has header "Authorization" set to a signature of the request', () => {
+          expect(options.headers).toBeTruthy();
+          expect(options.headers['Authorization']).toBe('Basic QUNqcFJIRnY3TDhpTjRxblZlUjRVN3B5aHpHeFNyNFoyOklNZlhIYlRSeTdGK1hiTnFwVTZBbDdvajVHckI0NHBubWpSNEU5SmUyeHk2WHV1TnpGUjVGaGZFUkNmWGh5WEpYUFZWVFlVVzdiY3pwclB2SXJhY1JpVT0=');
+        });
+      });
+    });
+
+    describe('when the response is not an array', () => {
+      it('rejects the returned promise with an error', () => {
+        global.fetch.mockImplementationOnce(() => Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve({})
+        }));
+
+        expect.hasAssertions();
+
+        return getContactRequests(pineAddress, mnemonic).catch((error) => {
+          expect(error).toBeTruthy();
+          expect(error.message).toContain('Unknown error');
+        });
+      });
+    });
+
+    describe('when the response is an error', () => {
+      it('rejects the returned promise with the error message from the response', () => {
+        global.fetch.mockImplementationOnce(() => Promise.resolve({
+          ok: false,
+          json: () => Promise.resolve({
+            message: 'eeaf4fc7-b505-4460-9cb5-363ecb4a716c'
+          })
+        }));
+
+        expect.hasAssertions();
+
+        return getContactRequests(pineAddress, mnemonic).catch((error) => {
+          expect(error).toBeTruthy();
+          expect(error.message).toBe('eeaf4fc7-b505-4460-9cb5-363ecb4a716c');
+        });
+      });
+    });
+  });
+});
--- src/PinePaymentProtocol/user/contactRequests/get.js
+++ src/PinePaymentProtocol/user/contactRequests/get.js
@@ -2,6 +2,14 @@ import { getKeyPairFromMnemonic, getUserIdFromPublicKey } from '../../crypto';
 import { parse as parseAddress, resolveBaseUrl } from '../../address';
 import { getAuthorizationHeader } from '../../authentication';
 
+/**
+ * Gets all contact requests for a Pine user.
+ *
+ * @param {string} pineAddress - Pine address to get contact requests for.
+ * @param {string} mnemonic - Mnemonic to authenticate the request with.
+ *
+ * @returns {Promise} A promise that resolves to an array of contact requests.
+ */
 const get = (pineAddress, mnemonic) => {
   const { hostname } = parseAddress(pineAddress);
   const keyPair = getKeyPairFromMnemonic(mnemonic);
