fe05d1f3c1f974a0fcd31e5df970f44c9aa9b916
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2019-02-24 23:55:18 +0100

    Test and document PinePaymentProtocol/authentication/getAuthorizationHeader.js

---
+++ __tests__/src/PinePaymentProtocol/authentication/getAuthorizationHeader.test.js
@@ -0,0 +1,24 @@
+import getKeyPairFromMnemonic from '../../../../src/PinePaymentProtocol/crypto/getKeyPairFromMnemonic';
+import getAuthorizationHeader from '../../../../src/PinePaymentProtocol/authentication/getAuthorizationHeader';
+
+describe('getAuthorizationHeader', () => {
+  it('is a function', () => {
+    expect(typeof getAuthorizationHeader).toBe('function');
+  });
+
+  it('accepts four arguments', () => {
+    expect(getAuthorizationHeader.length).toBe(4);
+  });
+
+  it('returns an HTTP Authorization header for the passed request details', () => {
+    const user = 'user1@pine.pm';
+    const path = '/v1/users';
+    const rawBody = '{ "test": true }';
+    const mnemonic = 'abuse boss fly battle rubber wasp afraid hamster guide essence vibrant tattoo';
+    const keyPair = getKeyPairFromMnemonic(mnemonic);
+    const actualHeader = getAuthorizationHeader(user, path, rawBody, keyPair);
+    const expectedHeader = 'Basic dXNlcjFAcGluZS5wbTpIemJSVmdSOTFRRjBScmV0c0xEQnIvL1UyU3lremJCdDR2R0R3ZktLN25ja0RoSG43c1plWlRFUFlrbk83SHBrWFdlOWc1ZkNScFlSYVR1dGxqVlRtVXc9';
+
+    expect(actualHeader).toBe(expectedHeader);
+  });
+});
--- src/PinePaymentProtocol/authentication/getAuthorizationHeader.js
+++ src/PinePaymentProtocol/authentication/getAuthorizationHeader.js
@@ -1,5 +1,17 @@
 import { sign } from '../crypto';
 
+/**
+ * Creates an HTTP Authorization header with user credentials
+ * consisting of a user ID or address and a signature of the
+ * request using the user's mnemonic.
+ *
+ * @param {string} user - User ID or Pine address of the user making the request.
+ * @param {string} path - HTTP path of the request to sign, e.g. `/v1/users`.
+ * @param {string} rawBody - The request body as it will be sent, e.g. as JSON.
+ * @param {object} keyPair - A bitcoinjs key pair to sign the request with.
+ *
+ * @returns {string} An HTTP Authorization header with user credentials.
+ */
 const getAuthorizationHeader = (user, path, rawBody, keyPair) => {
   const message = path + rawBody;
   const signature = sign(message, keyPair);
