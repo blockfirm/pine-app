585780c8870ba1d8f47a6b84624eea69643f7d52
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2019-04-25 22:51:17 +0200

    Load existing messages from persistent storage before adding

--- src/actions/messages/add.js
+++ src/actions/messages/add.js
@@ -1,6 +1,17 @@
 import { save } from './save';
+import { load } from './load';
 
 export const MESSAGES_ADD = 'MESSAGES_ADD';
+
+const loadExistingMessages = (contactId, dispatch, state) => {
+  const messages = state.messages.itemsByContact[contactId];
+
+  if (messages) {
+    return Promise.resolve();
+  }
+
+  return dispatch(load(contactId));
+};
 
 /**
  * Action to add a message to a contact's conversation.
@@ -11,17 +22,25 @@ export const MESSAGES_ADD = 'MESSAGES_ADD';
  * @returns {Promise} A promise that resolves when the message has been added.
  */
 export const add = (contactId, message) => {
-  return (dispatch) => {
-    /**
-     * The message is added to the state by the reducer so this
-     * action must be dispatched before saving the state.
-     */
-    dispatch({
-      type: MESSAGES_ADD,
-      contactId,
-      message
-    });
+  return (dispatch, getState) => {
+    const state = getState();
 
-    return dispatch(save(contactId));
+    /**
+     * Load existing messages from persistent storage
+     * before adding the new message.
+     */
+    return loadExistingMessages(contactId, dispatch, state).then(() => {
+      /**
+       * The message is added to the state by the reducer so this
+       * action must be dispatched before saving the state.
+       */
+      dispatch({
+        type: MESSAGES_ADD,
+        contactId,
+        message
+      });
+
+      return dispatch(save(contactId));
+    });
   };
 };
