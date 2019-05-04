bf88af1dc26f175a048dee81eb31d6d0d8131c72
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2019-05-04 19:08:42 +0200

    Show user's avatar in middle of QR code

--- src/screens/ReceiveScreen.js
+++ src/screens/ReceiveScreen.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { StyleSheet, Clipboard, Share, View, Dimensions } from 'react-native';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import QRCode from 'react-qr-code';
+import QRCode from 'react-native-qrcode-svg';
 
 import BaseScreen from './BaseScreen';
+import { parse as parseAddress, resolveBaseUrl } from '../pineApi/address';
 import ReceiveScreenHeader from '../components/ReceiveScreenHeader';
 import ContentView from '../components/ContentView';
 import Paragraph from '../components/Paragraph';
 import AddressLabel from '../components/AddressLabel';
+
+const AVATAR_PLACEHOLDER = require('../images/AvatarPlaceholder.png');
 
 const WINDOW_WIDTH = Dimensions.get('window').width;
 const QR_CODE_WIDTH = WINDOW_WIDTH - 80 - 32;
@@ -34,6 +37,7 @@ const styles = StyleSheet.create({
 });
 
 @connect((state) => ({
+  avatar: state.settings.user.profile.avatar,
   pineAddress: state.settings.user.profile.address,
   address: state.bitcoin.wallet.addresses.external.unused
 }))
@@ -45,6 +49,20 @@ export default class ReceiveScreen extends Component {
   _getBitcoinUri() {
     const { pineAddress, address } = this.props;
     return `bitcoin:${address}?pine=${pineAddress}`;
+  }
+
+  _getAvatarSource() {
+    const { pineAddress, avatar } = this.props;
+
+    if (!avatar) {
+      return AVATAR_PLACEHOLDER;
+    }
+
+    const { username, hostname } = parseAddress(pineAddress);
+    const baseUrl = resolveBaseUrl(hostname);
+    const uri = `${baseUrl}/v1/users/${username}/avatar?byUsername=1&c=${avatar.checksum}`;
+
+    return { uri };
   }
 
   _shareAddress() {
@@ -66,7 +84,14 @@ export default class ReceiveScreen extends Component {
 
           <View>
             <AddressLabel address={pineAddress} shorten={false} textStyle={styles.pineAddress} tooltipArrowDirection='down' />
-            <QRCode value={qrData} size={QR_CODE_WIDTH} />
+            <QRCode
+              value={qrData}
+              size={QR_CODE_WIDTH}
+              logo={this._getAvatarSource()}
+              logoSize={80}
+              logoBorderRadius={40}
+              color='#111111'
+            />
             <AddressLabel address={address} textStyle={styles.address} tooltipArrowDirection='up' />
           </View>
 
@@ -79,6 +104,9 @@ export default class ReceiveScreen extends Component {
 
 ReceiveScreen.propTypes = {
   dispatch: PropTypes.func,
+  avatar: PropTypes.shape({
+    checksum: PropTypes.string
+  }),
   pineAddress: PropTypes.string,
   address: PropTypes.string,
   onBackPress: PropTypes.func
