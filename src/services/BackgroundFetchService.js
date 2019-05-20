7dd0fd2d4aaea2c4e82ad5e1c95de428e9eb51ab
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2019-05-21 01:41:15 +0200

    Implement background fetch to sync in the background
    This is a complement to the background fetch triggered by
    notifications which should make it more reliable by also
    background fetching with an interval of one hour.
    
    This provides the following benefits:
    
    * Transactions should be redeemed without starting the app
    * Change would be returned to the sender without the recipient
      having to do anything

---
+++ src/services/BackgroundFetchService.js
@@ -0,0 +1,48 @@
+import BackgroundFetch from 'react-native-background-fetch';
+import { sync as syncApp } from '../actions';
+
+const MINIMUM_FETCH_INTERVAL = 60; // In minutes (minimum 15).
+
+export default class BackgroundFetchService {
+  constructor(store) {
+    this.store = store;
+  }
+
+  _shouldSync() {
+    const state = this.store.getState();
+    const { initialized } = state.settings;
+    const { hasAcceptedTerms } = state.settings.user;
+    const { disconnected } = state.network.internet;
+
+    // Only sync if connected to the internet and has a wallet.
+    return !disconnected && initialized && hasAcceptedTerms;
+  }
+
+  start() {
+    const { dispatch } = this.store;
+
+    const config = {
+      minimumFetchInterval: MINIMUM_FETCH_INTERVAL
+    };
+
+    BackgroundFetch.configure(config, () => {
+      if (this._shouldSync()) {
+        dispatch(syncApp())
+          .then(() => {
+            BackgroundFetch.finish(BackgroundFetch.FETCH_RESULT_NEW_DATA);
+          })
+          .catch(() => {
+            BackgroundFetch.finish(BackgroundFetch.FETCH_RESULT_FAILED);
+          });
+      } else {
+        BackgroundFetch.finish(BackgroundFetch.FETCH_RESULT_FAILED);
+      }
+    }, () => {
+      // BackgroundFetch failed to start.
+    });
+  }
+
+  stop() {
+
+  }
+}
--- src/services/ServiceManager.js
+++ src/services/ServiceManager.js
@@ -1,10 +1,12 @@
 import AutoSyncService from './AutoSyncService';
+import BackgroundFetchService from './BackgroundFetchService';
 import ConnectionStatusService from './ConnectionStatusService';
 import FiatRatesService from './FiatRatesService';
 import NotificationService from './NotificationService';
 
 const serviceClasses = [
   AutoSyncService,
+  BackgroundFetchService,
   ConnectionStatusService,
   FiatRatesService,
   NotificationService
