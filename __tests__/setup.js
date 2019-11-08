4e8f7d2c556aa52a016716e1739d1f50a40aea4b
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2019-11-08 23:39:58 +0100

    Upgrade React Native to v0.61.4

--- __tests__/setup.js
+++ __tests__/setup.js
@@ -1,20 +1,77 @@
-import { NativeModules } from 'react-native';
-
-jest.mock('Settings', () => ({
-  get: jest.fn(() => ({})),
-  set: jest.fn(() => {})
-}));
-
-jest.mock('AccessibilityInfo', () => ({
-  addEventListener: jest.fn(),
-  removeEventListener: jest.fn(),
-  isReduceTransparencyEnabled: jest.fn(() => Promise.resolve(false))
-}));
+import * as ReactNative from 'react-native';
 
 jest.mock('../src/config');
 
-NativeModules.SettingsManager = {
-  settings: {
-    AppleLocale: 'en_US'
-  }
-};
+jest.mock('react-native-vector-icons/Ionicons', () => 'IoniconIcon');
+jest.mock('react-native-vector-icons/Octicons', () => 'OcticonIcon');
+jest.mock('react-native-vector-icons/Entypo', () => 'EntypoIcon');
+
+jest.mock('react-native/Libraries/TurboModule/TurboModuleRegistry', () => {
+  const TurboModuleRegistry = require.requireActual(
+    'react-native/Libraries/TurboModule/TurboModuleRegistry'
+  );
+
+  return {
+    ...TurboModuleRegistry,
+    getEnforcing: function (name) {
+      if (name === 'SettingsManager') {
+        const NativeSettingsManager = require.requireActual(
+          'react-native/Libraries/Settings/NativeSettingsManager'
+        );
+
+        return {
+          ...NativeSettingsManager,
+          getConstants: () => ({ settings: {} })
+        };
+      }
+
+      return TurboModuleRegistry.getEnforcing(...arguments);
+    }
+  };
+});
+
+jest.mock('react-native/Libraries/Settings/Settings', () => {
+  const Settings = require.requireActual(
+    'react-native/Libraries/Settings/Settings'
+  );
+
+  return {
+    ...Settings,
+    get: jest.fn(() => ({})),
+    set: jest.fn(() => {})
+  };
+});
+
+jest.mock('react-native/Libraries/Components/AccessibilityInfo/AccessibilityInfo', () => {
+  const AccessibilityInfo = require.requireActual(
+    'react-native/Libraries/Components/AccessibilityInfo/AccessibilityInfo'
+  );
+
+  return {
+    ...AccessibilityInfo,
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    isReduceTransparencyEnabled: jest.fn(() => Promise.resolve(false))
+  };
+});
+
+jest.doMock('react-native', () => {
+  const NativeSettingsManager = require.requireActual(
+    'react-native/Libraries/Settings/NativeSettingsManager'
+  );
+
+  return Object.setPrototypeOf(
+    {
+      NativeModules: {
+        ...ReactNative.NativeModules,
+        SettingsManager: {
+          ...NativeSettingsManager,
+          settings: {
+            AppleLocale: 'en_US'
+          }
+        }
+      }
+    },
+    ReactNative
+  );
+});
