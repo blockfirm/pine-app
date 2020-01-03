cea32f5304f99b981ce3370e38a2f6dda8bfea0f
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2020-01-03 17:58:43 +0100

    Determine correct default time formatting options

--- src/localization/formatTime.js
+++ src/localization/formatTime.js
@@ -4,19 +4,33 @@ import LOCALE from './locale';
 const FORCE_12_HOUR_TIME = NativeModules.SettingsManager.settings.AppleICUForce12HourTime || false;
 const FORCE_24_HOUR_TIME = NativeModules.SettingsManager.settings.AppleICUForce24HourTime || false;
 
-const formatTime = (date) => {
-  const formatOptions = {
-    hour: 'numeric',
-    minute: '2-digit'
+const NUMERIC = 'numeric';
+const TWO_DIGIT = '2-digit';
+
+const getDefaultFormatOptions = () => {
+  const formattedTime = new Date('1 Jan 2020 01:01').toLocaleTimeString(LOCALE);
+  const timeParts = formattedTime.split(':');
+  const hour = timeParts[0];
+  const minute = timeParts[1];
+
+  return {
+    hour: hour.length === 2 ? TWO_DIGIT : NUMERIC,
+    minute: minute.length === 2 ? TWO_DIGIT : NUMERIC
   };
+};
+
+const DEFAULT_FORMAT_OPTIONS = getDefaultFormatOptions();
+
+const formatTime = (date) => {
+  const formatOptions = { ...DEFAULT_FORMAT_OPTIONS };
 
   if (FORCE_12_HOUR_TIME) {
-    formatOptions.hour = 'numeric';
+    formatOptions.hour = NUMERIC;
     formatOptions.hour12 = true;
   }
 
   if (FORCE_24_HOUR_TIME) {
-    formatOptions.hour = '2-digit';
+    formatOptions.hour = TWO_DIGIT;
     formatOptions.hour12 = false;
   }
 
