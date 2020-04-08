a2962e4c80635787102f4cab73ec566ffc22272f
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2020-04-08 22:25:54 +0200

    Merge ConnectionStatus.js with ConnectionStatusContainer.js

--- src/containers/ConnectionStatusContainer.js
+++ src/containers/ConnectionStatusContainer.js
@@ -3,21 +3,25 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import { getInfo as getServerInfo } from '../actions/network/server/getInfo';
-import ConnectionStatus from '../components/ConnectionStatus';
+import Status from '../components/Status';
 
 const UPDATE_INTERVAL = 3000;
 
-const mapStateToProps = (state) => {
-  return {
-    settings: state.settings,
-    serverInfo: state.network.server.info
-  };
-};
+const LABEL_NOT_CONNECTED = 'Not Connected';
+const LABEL_CONNECTED = 'Connected';
+const LABEL_INCONSISTENT_NETWORKS = 'Inconsistent Networks';
+const LABEL_SERVER_NOT_CONNECTED_TO_NODE = 'Server Not Connected to Node';
+
+const mapStateToProps = (state) => ({
+  settings: state.settings,
+  serverInfo: state.network.server.info
+});
 
 class ConnectionStatusContainer extends Component {
   static propTypes = {
     dispatch: PropTypes.func,
-    settings: PropTypes.object
+    settings: PropTypes.object,
+    serverInfo: PropTypes.object
   };
 
   _updateServerStatus() {
@@ -36,6 +40,10 @@ class ConnectionStatusContainer extends Component {
     }, UPDATE_INTERVAL);
   }
 
+  componentWillUnmount() {
+    clearInterval(this._updateInterval);
+  }
+
   componentDidUpdate(prevProps) {
     const { settings } = this.props;
 
@@ -44,13 +52,27 @@ class ConnectionStatusContainer extends Component {
     }
   }
 
-  componentWillUnmount() {
-    clearInterval(this._updateInterval);
-  }
-
   render() {
+    const { settings, serverInfo } = this.props;
+    let status = Status.STATUS_ERROR;
+    let label = LABEL_NOT_CONNECTED;
+
+    if (serverInfo) {
+      if (serverInfo.isConnected) {
+        if (serverInfo.network === settings.bitcoin.network) {
+          status = Status.STATUS_OK;
+          label = LABEL_CONNECTED;
+        } else {
+          status = Status.STATUS_WARNING;
+          label = LABEL_INCONSISTENT_NETWORKS;
+        }
+      } else if (serverInfo.isConnected === false) {
+        label = LABEL_SERVER_NOT_CONNECTED_TO_NODE;
+      }
+    }
+
     return (
-      <ConnectionStatus {...this.props} />
+      <Status status={status} label={label} />
     );
   }
 }
