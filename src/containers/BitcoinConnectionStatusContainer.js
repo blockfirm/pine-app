dbf36499102461878b29df1e2791326908ac9745
Timothy Engqvist Johansson <timothy.ejohansson@gmail.com>
2020-04-08 23:39:31 +0200

    Include syncing errors when checking bitcoin service status

--- src/containers/BitcoinConnectionStatusContainer.js
+++ src/containers/BitcoinConnectionStatusContainer.js
@@ -11,17 +11,20 @@ const LABEL_NOT_CONNECTED = 'Not Connected';
 const LABEL_CONNECTED = 'Connected';
 const LABEL_INCONSISTENT_NETWORKS = 'Inconsistent Networks';
 const LABEL_SERVER_NOT_CONNECTED_TO_NODE = 'Server Not Connected to Node';
+const LABEL_SYNC_ERROR = 'Syncing Error';
 
 const mapStateToProps = (state) => ({
   settings: state.settings,
-  serverInfo: state.network.server.info
+  serverInfo: state.network.server.info,
+  syncError: Boolean(state.network.server.disconnected)
 });
 
 class BitcoinConnectionStatusContainer extends Component {
   static propTypes = {
     dispatch: PropTypes.func,
     settings: PropTypes.object,
-    serverInfo: PropTypes.object
+    serverInfo: PropTypes.object,
+    syncError: PropTypes.bool
   };
 
   _updateServerStatus() {
@@ -53,18 +56,21 @@ class BitcoinConnectionStatusContainer extends Component {
   }
 
   render() {
-    const { settings, serverInfo } = this.props;
+    const { settings, serverInfo, syncError } = this.props;
     let status = Status.STATUS_ERROR;
     let label = LABEL_NOT_CONNECTED;
 
     if (serverInfo) {
       if (serverInfo.isConnected) {
-        if (serverInfo.network === settings.bitcoin.network) {
-          status = Status.STATUS_OK;
-          label = LABEL_CONNECTED;
-        } else {
+        if (serverInfo.network !== settings.bitcoin.network) {
           status = Status.STATUS_WARNING;
           label = LABEL_INCONSISTENT_NETWORKS;
+        } else if (syncError) {
+          status = Status.STATUS_ERROR;
+          label = LABEL_SYNC_ERROR;
+        } else {
+          status = Status.STATUS_OK;
+          label = LABEL_CONNECTED;
         }
       } else if (serverInfo.isConnected === false) {
         label = LABEL_SERVER_NOT_CONNECTED_TO_NODE;
