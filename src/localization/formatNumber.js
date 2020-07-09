10ec8f6d89da00ff8e23c9e25cddb115091a0f08
Timothy Engqvist Johansson <timothy@blockfirm.se>
2020-07-09 22:50:24 +0200

    Specify English as language locale when formatting numbers
    This fixes a bug that allowed multiple decimal separators to be inserted.

--- src/localization/formatNumber.js
+++ src/localization/formatNumber.js
@@ -1,12 +1,19 @@
 import REGION_LOCALE from './regionLocale';
 
+/**
+ * Hardcode English as language because that's the only
+ * language supported at the moment.
+ */
+const LANGUAGE_LOCALE = 'en';
+const LOCALE = `${LANGUAGE_LOCALE}-${REGION_LOCALE}`;
+
 const formatNumber = (number) => {
   const formatOptions = {
     style: 'decimal',
     maximumFractionDigits: 20
   };
 
-  return new Intl.NumberFormat(REGION_LOCALE, formatOptions).format(number);
+  return new Intl.NumberFormat(LOCALE, formatOptions).format(number);
 };
 
 export default formatNumber;
